129efb8689218154bef9d0e4101e2ffc490e8d4c
jinzewen001 <jinzewen001@ke.com>
2025-01-07 23:22:36 +0800

    dev

--- src/uploader/imgur/ImgurAuthenticatedUploader.ts
+++ src/uploader/imgur/ImgurAuthenticatedUploader.ts
@@ -7,4 +7,10 @@ export default class ImgurAuthenticatedUploader implements ImageUploader {
   async upload(image: File, albumId?: string): Promise<string> {
     return (await this.client.upload(image, albumId)).data.link
   }
+
+  async download(imageURL: ImageURL): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        reject(new Error(`Not Implement!!!`));
+    });
+  }
 }
